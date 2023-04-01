65c6e7e093ead93f92bc4ffdc4326b14375a3d16
costaalacuparmare <vladco2003@gmail.com>
2023-04-02 01:01:24 +0300

    msk

--- IAP1/curs.py/main.py
+++ IAP1/curs.py/main.py
@@ -1,11 +1,19 @@
-# This is a sample Python script.
+#lucruri invatate la curs
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# importam clasa Flask
+from flask import Flask
+# cream o instanta a clasei
+app = Flask(__name__)
+# specificam ce URL ne va apela functia
+@app.route("/")
+def hello_world():
+ # returnam un mesaj in browser (default HTML)
+ return "<p>Hello, World!</p>"
+# rulam serverul de Flask
+if __name__ == "__main__":
+ app.run(host="0.0.0.0")
 
 def print_hi(name):
-
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
@@ -14,3 +22,4 @@ if __name__ == '__main__':
     print_hi('Dan Coi')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
