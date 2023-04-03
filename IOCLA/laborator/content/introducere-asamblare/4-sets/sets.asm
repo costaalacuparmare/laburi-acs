45367599f7299b685c9b4c3447882d48015cfe68
costaalacuparmare <vladco2003@gmail.com>
2023-04-03 15:52:19 +0300

    msj

--- IOCLA/laborator/content/introducere-asamblare/4-sets/sets.asm
+++ IOCLA/laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -12,19 +12,32 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
     ; TODO1: reuniunea a doua multimi
-
+    mov edx, eax
+    or edx, ebx
+    PRINTF32 `Reuniunea este: \x0`
+    PRINTF32 `%u\n\x0`, edx
 
     ; TODO2: adaugarea unui element in multime
-
+    or eax, 0x300
+    PRINTF32 `%u\n\x0`, eax
 
     ; TODO3: intersectia a doua multimi
-
+    mov edx, eax
+    and edx, ebx
+    PRINTF32 `Intersectia este: \x0`
+    PRINTF32 `%u\n\x0`, edx
 
     ; TODO4: complementul unei multimi
-
+    mov edx, eax
+    not edx
+    PRINTF32 `%u\n\x0`, edx
 
     ; TODO5: eliminarea unui element
-
+    mov edx, eax
+    mov
+    not edx
+    and eax, edx
+    PRINTF32 `%u\n\x0`, eax
 
     ; TODO6: diferenta de multimi EAX-EBX
 
