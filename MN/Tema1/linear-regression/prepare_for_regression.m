5e96befcaf3d92af1ce83c6ceaf1b926d06b6789
costaalacuparmare <vladco2003@gmail.com>
2023-05-06 14:42:36 +0300

    msj

--- MN/Tema1/linear-regression/prepare_for_regression.m
+++ MN/Tema1/linear-regression/prepare_for_regression.m
@@ -2,6 +2,14 @@ function [FeatureMatrix] = prepare_for_regression(InitialMatrix)
   % InitialMatrix -> the matrix that must be transformed
 
   % FeatureMatrix -> the matrix with all training examples
-  
+
   % TODO: prepare_for_regression implementation
+  [m n] = size(InitialMatrix);
+  for i = 1:m
+    for j = 1:n
+      if (strcmp(InitialMatrix(i,j), "yes"))
+        FeatureMatrix(i,j) = 1;
+      endif
+    endfor
+  endfor
 endfunction
