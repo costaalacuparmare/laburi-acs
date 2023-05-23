d995481e8584e31af9048456081883e13e608293
costaalacuparmare <vladco2003@gmail.com>
2023-05-23 23:08:17 +0300

    msj

--- SD/Tema3/task2.c
+++ SD/Tema3/task2.c
@@ -19,7 +19,76 @@ int getVerticesDepth(FILE *input, TVertices *vertices_array, int nr_vertices)
 	return treasure_weight;
 }
 
+#define INFINITE 9999
+#define MAX_VERTICES 100
+
+void Dijkstra(TGraph G, int startVertex, int endVertex, TVertices *vertices_array, FILE *output) {
+	int distance[MAX_VERTICES];
+	int visited[MAX_VERTICES];
+	int previous[MAX_VERTICES];
+	int path[MAX_VERTICES];
+	int pathLength = 0;
+	int i, minDist, nextVertex;
+
+	for (i = 0; i < G->nr_vertices; i++) {
+		distance[i] = INFINITE;
+		visited[i] = 0;
+		previous[i] = -1;
+	}
+
+	distance[startVertex] = 0;
+
+	while (!visited[endVertex]) {
+		minDist = INFINITE;
+
+		for (i = 0; i < G->nr_vertices; i++) {
+			if (!visited[i] && distance[i] < minDist) {
+				minDist = distance[i];
+				nextVertex = i;
+			}
+		}
+
+		visited[nextVertex] = 1;
+
+		TEdge edge = G->list_array[nextVertex];
+		while (edge != NULL) {
+			int neighbor = edge->dest;
+			int cost = edge->cost;
+
+			if (!visited[neighbor] && distance[nextVertex] + cost < distance[neighbor]) {
+				distance[neighbor] = distance[nextVertex] + cost;
+				previous[neighbor] = nextVertex;
+			}
+
+			edge = edge->next;
+		}
+	}
+
+	int currentVertex = endVertex;
+	while (currentVertex != startVertex) {
+		path[pathLength] = currentVertex;
+		pathLength++;
+		currentVertex = previous[currentVertex];
+	}
+	path[pathLength] = startVertex;
+	pathLength++;
+
+	for (i = pathLength - 1; i >= 0; i--) {
+		fprintf(output, "%s ", vertices_array[path[i]]->name);
+	}
+	fprintf(output, "\n");
+	fprintf(output, "%d\n", distance[endVertex]);
+}
+
+
 void task2(TGraph *graph, TVertices *vertices_array, int treasure_weight, FILE *output)
 {
 	printf("%d\n", treasure_weight);
+	int startVertex = 0;
+	int endVertex = 0;
+	for (int i = 0; i < (*graph)->nr_vertices; i++)
+		if (strstr("Corabie\n", vertices_array[i]->name))
+			endVertex = vertices_array[i]->code;
+
+	Dijkstra((*graph), startVertex, endVertex, vertices_array, output);
 }
