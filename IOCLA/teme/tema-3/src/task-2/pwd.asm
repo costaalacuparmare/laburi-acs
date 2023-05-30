15af63367443b69765c794fe11a254ee53a3ac13
costaalacuparmare <vladco2003@gmail.com>
2023-05-30 13:17:56 +0300

    msj

--- IOCLA/teme/tema-3/src/task-2/pwd.asm
+++ IOCLA/teme/tema-3/src/task-2/pwd.asm
@@ -1,3 +1,5 @@
+%include "../printf32.asm"
+
 section .data
 	back db "..", 0
 	curr db ".", 0
@@ -6,13 +8,110 @@ section .data
 
 section .text
 	global pwd
-	extern strcpy
-	extern strlen
+	extern strcmp
+	extern printf
+	extern strcat
 ;;	void pwd(char **directories, int n, char *output)
 ;	Adauga in parametrul output path-ul rezultat din
 ;	parcurgerea celor n foldere din directories
 pwd:
 	enter 0, 0
+	pusha
+	xor eax, eax       ; current position in directories array
+	xor ebx, ebx       ; current position in output
+	xor edx, edx       ; temporary register
 
+	; get arguments
+	mov esi, [ebp + 8] ; directories
+	mov ecx, [ebp + 12] ; n
+	mov edi, [ebp + 16] ; output
+
+add_directory:
+
+	; check if all directories have been processed
+	cmp ecx, 0
+	je end_pwd
+
+	; decrement the number of directories
+	dec ecx
+
+	; get the current directory name
+	mov edx, dword [esi + eax * 4]
+
+	; push values modified by strcmp
+	push eax
+	push ecx
+	push edx
+
+	; call strcmp
+	push edx
+	push curr
+	call strcmp
+	add esp, 8
+
+	; check if the current directory is "."
+	cmp eax, 0
+	je next_directory
+
+	; get back the current directory
+	pop edx
+	push edx
+
+	; call strcmp
+    push edx
+    push back
+    call strcmp
+    add esp, 8
+	; check if the current directory is ".."
+	cmp eax, 0
+    je prev_directory
+
+	; add '/' in output
+    push edi
+    push slash
+    call strcat
+    add esp, 8
+    mov edi, eax
+
+	; get back the current directory
+	;pop edx
+    ;push edx
+
+	; add current directory in output
+	;push edi
+	;push edx
+	;call strcat
+	;add esp, 8
+	;mov edi, eax
+
+	pop edx
+    pop ecx
+    pop eax
+
+	inc eax
+	jmp add_directory
+
+
+
+next_directory:
+	; pop values modified by strcmp
+	pop edx
+    pop ecx
+    pop eax
+
+	inc eax
+	jmp add_directory
+
+prev_directory:
+	; pop values modified by strcmp
+	pop edx
+	pop ecx
+	pop eax
+
+	inc eax
+    jmp add_directory
+
+end_pwd:
+	popa
 	leave
 	ret
--- IOCLA/teme/tema-3/src/task-3/sort-words.asm
+++ IOCLA/teme/tema-3/src/task-3/sort-words.asm
@@ -1,7 +1,7 @@
 global get_words
 global compare_func
 global sort
-
+extern strtok
 section .text
 
 ;; get_words(char *s, char **words, int number_of_words)
@@ -9,6 +9,60 @@ section .text
 ;  number_of_words reprezinta numarul de cuvinte
 get_words:
     enter 0, 0
+	pusha
 
+	; get arguments
+	mov esi, [ebp + 8] ; s
+	mov edi, [ebp + 12] ; words
+	mov ecx, [ebp + 16] ; nr_words
+
+	xor edx, edx ; counter for words array
+
+first_strtok:
+	push edx
+	push delimiters
+    push esi
+    call strtok
+    add esp, 8
+	pop edx
+    cmp eax, 0
+    je end_get_words
+
+    mov [edi + edx * 4], eax
+    inc edx
+    dec ecx
+
+strtok_loop:
+	push edx
+	push delimiters
+    push 0
+    call strtok
+    add esp, 8
+	pop edx
+	cmp eax, 0
+	jz end_get_words
+
+	mov [edi + edx * 4], eax
+	inc edx
+
+	dec ecx
+	cmp ecx, 0
+	jg strtok_loop
+
+end_get_words:
+	popa
     leave
     ret
+    delimiters db " .,", 10, 0 ; defines the delimiter string
+
+;; sort(char **words, int number_of_words, int size)
+;  functia va trebui sa apeleze qsort pentru soratrea cuvintelor
+;  dupa lungime si apoi lexicografix
+sort:
+	enter 0, 0
+	pusha
+
+	popa
+    leave
+    ret
+
