4de4cdb59897a7e8e421ff2c0e83210e7cb186c0
costaalacuparmare <vladco2003@gmail.com>
2023-06-04 23:20:16 +0300

    C Programming Language

--- PCLP/Headers/flab4.h
+++
@@ -1,8 +0,0 @@
-int fact_i ( int n);
-int fact_r ( int n);
-int sumcif_i ( int n);
-int sumcif_r ( int n);
-int prim ( int n);
-int palindrom( int n);
-void divpalindrom ( int n);
-void sirprim ( int contor);
