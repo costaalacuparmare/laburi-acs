5500c0206ef0039a4b9afc8ef52cfd743b2cdb92
costaalacuparmare <vconstantinescu@protonmail.com>
2023-09-09 19:42:48 +0300

    msj

--- MN/Functii/numerical_integration.m
+++
@@ -1,6 +0,0 @@
-function integral = numerical_integration(f, a, b, N)
-    % Metoda aproximativă de integrare
-    x = linspace(a, b, N+1);
-    f_vals = f(x);
-    integral = sum(f_vals) * (b - a) / N;
-end
