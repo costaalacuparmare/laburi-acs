c6c5d07e8a265ac88269203b48c1cfa81abd68eb
costaalacuparmare <vconstantinescu@protonmail.com>
2023-10-13 13:14:20 +0300

    Computer Management & Assembly Language

--- IOCLA/laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
+++ IOCLA/laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
@@ -11,17 +11,23 @@ get_max:
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
-
+    ; [ebp+16] is max position
 	mov ebx, [ebp+8]
 	mov ecx, [ebp+12]
+	mov edx, [ebp+16]
 	xor eax, eax
-
 compare:
 	cmp eax, [ebx+ecx*4-4]
 	jge check_end
 	mov eax, [ebx+ecx*4-4]
+	mov edx, ecx
 check_end:
 	loopnz compare
+
+    dec edx
+
+    mov ebx, [ebp+16]
+    mov [ebx], edx
 
 	pop ebx
 
--- IOCLA/laborator/content/interactiune-c-assembly/3-stack-frame/print-hello.asm
+++ IOCLA/laborator/content/interactiune-c-assembly/3-stack-frame/print-hello.asm
@@ -10,10 +10,11 @@ global print_hello
 ;   TODO: Adăugați instrucțiunea lipsă
 print_hello:
 	push ebp
+	mov ebp, esp
 
 	push message
 	call printf
-	add esp, 4
+	sub esp, 4
 
 	leave
 	ret
