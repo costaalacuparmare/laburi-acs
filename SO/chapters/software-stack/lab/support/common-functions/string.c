ebe74bd73468364125812af7884adb0980837d33
costaalacuparmare <vconstantinescu@protonmail.com>
2023-10-20 21:38:26 +0300

    msj

---
+++ SO/chapters/software-stack/lab/support/common-functions/string.c
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include "string.h"
+
+unsigned long strlen(const char *s)
+{
+	unsigned long len;
+
+	for (len = 0; *s != '\0'; s++, len++) { }
+
+	return len;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+	char *d;
+
+	for (d = dest; *src != '\0'; src++, d++)
+		*d = *src;
+	/* Also place NUL byte. */
+	*d = *src;
+
+	return dest;
+}
+
+char *strcat(char *dest, const char *src)
+{
+    char* ptr = dest + strlen(dest);
+    while (*src != '\0') {
+        *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return dest;
+}
---
+++ SO/chapters/software-stack/lab/support/syscall-wrapper/main.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include "./syscall.h"
+
+int main(void)
+{
+    char buf[9];
+    buf[9] = '\0';
+    int count = read(0, buf, 8);
+	write(1, buf, count);
+	exit(0);
+	return 0;
+}
