316e62ac454df8770b9e2adc8b39e5ea51dec5ce
costaalacuparmare <vconstantinescu@protonmail.com>
2024-10-14 13:03:12 +0300

    Structure and Operation of Computers

--- SOC/lab03/lab03.ip_user_files/sim_scripts/cronometru/xcelium/run.f
+++
@@ -1,8 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../bd/cronometru/ip/cronometru_numarator_59_0_0_0/sim/cronometru_numarator_59_0_0_0.v" \
-  "../../../bd/cronometru/sim/cronometru.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
--- SOC/lab05/lab05.srcs/sources_1/new/transfer_op.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module transfer_op(
-    input op,
-    output out
-    );
-    
-    assign out = op;
-endmodule
