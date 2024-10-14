66bcec0bb0b8d18df4b1202af5b25f4b2b384d78
costaalacuparmare <vconstantinescu@protonmail.com>
2024-10-14 12:54:56 +0300

    work in progress

--- PA/utils/default_checker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-diff --ignore-all-space -nq "$2" "$3" &>/dev/null
--- PA/utils/tutorial_compile_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make_helper() {
-    make -C "$(dirname $1)" $2
-}
-
-for makefile_path in $(find tutorial/ -name Makefile); do
-    make_helper "${makefile_path}" "$1" || exit 1
-done
