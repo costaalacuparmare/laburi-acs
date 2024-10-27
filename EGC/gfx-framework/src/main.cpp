ce2a15703686fa5a3bf1bbba0506776045025cba
costaalacuparmare <vconstantinescu@protonmail.com>
2024-10-27 20:58:33 +0200

    work in progress

--- EGC/gfx-framework/src/main.cpp
+++ EGC/gfx-framework/src/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
     // Create a new 3D world and start running it
     // World *world = new gfxc::SimpleScene();
 //    World *world = new m1::Lab3_Vis2D();
-    World *world = new m1::Lab3();
+    World *world = new m1::Lab3_Vis2D();
 
     world->Init();
     world->Run();
