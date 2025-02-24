20022b3406b4bdf14bcfab537e4139da77fd1d44
costaalacuparmare <vconstantinescu@protonmail.com>
2025-02-24 14:59:23 +0200

    gone

--- ISC/lab01/ex3/ex3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-files=$(find . -type f -size 987c)
-
-for file in $files; do
-	cat "$file"
-	echo
-done
--- ISC/lab01/ex4/ex4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-file 04-File
-gunzip -c 04-File > 01-File
-cat 01-File
